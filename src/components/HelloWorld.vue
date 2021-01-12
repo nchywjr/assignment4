242243b27d51cb306b99f9bfc38dc3fa4e005d5c
nchywjr <natchaya.wot@gmail.com>
2021-01-12 09:49:26 +0700

    first time

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <v-row>
+      <v-col>
+        <v-btn @click="fetchUsers">Fetch</v-btn>
+      </v-col>
+    </v-row>
+        <v-row>
+            <v-col>
+                <v-data-table 
+                :headers="headers"
+                :items="users"
+                :items-per-page="10"
+                >
+                </v-data-table>
+            </v-col>
+        </v-row>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+
+const uri = "https://api.coingecko.com/api/v3/coins/list"
+
+export default {
+    data(){
+        return {
+            headers:[
+                {
+                    text: 'ID',
+                    value: 'id'
+                },
+                {
+                    text: 'Symbol',
+                    value: 'symbol'
+                },
+                {
+                    text: 'Name',
+                    value: 'name'
+                },
+            ],
+
+            users: []
+        }
+    }, methods: {
+        async fetchUsers(){
+            const res = await axios.get(uri)
+            this.users = res.data
+        }
+    },
+}
+</script>
